1bde49d122553d0430d748f578f75e4e8e2f3df9
RGomes <66396248+ReFGomes@users.noreply.github.com>
2024-03-10 19:47:12 -0300

    Add files via upload

---
+++ cypress-desafio/cypress/e2e/criarConta.cy.js
@@ -0,0 +1,11 @@
+describe('Criar contas e fazer transação entre elas', () => {
+    it.only('deve registrar uma conta com saldo', () => {
+
+        cy.createNewUser1('')
+        cy.createNewUser2('')
+        cy.login2('')
+
+    })
+
+
+})
